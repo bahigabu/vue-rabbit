3bf8d241e653d5e78dd0af9d3834c76adcc78f8a
bahigabu <sbz1212lucky@gmail.com>
2023-06-08 00:44:00 +0800

    element主題色定製

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,37 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+// 下面3個是ElementPlus
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+// 上面3個是ElementPlus
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
+  plugins: [
+    vue(),
     // ...
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
-    })
+      // 採用sass配色系統
+      resolvers: [ElementPlusResolver({ importStyle: "sass" })],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自動導入定製化樣式文件
+        additionalData:`@use "@/styles/element/index.scss" as *;`
+      }
     }
   }
-})
+});
